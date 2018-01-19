dcda40f18528a37da4550339419bdfa4af25c070
Salman* <salman1m22@gmail.com>
2018-01-19 11:50:55 +0600

    test

--- src/main/groovy/org/grails/plugin/scaffolding/angular/markup/AngularContextMarkupRenderer.groovy
+++ src/main/groovy/org/grails/plugin/scaffolding/angular/markup/AngularContextMarkupRenderer.groovy
@@ -15,7 +15,7 @@ class AngularContextMarkupRenderer extends ContextMarkupRendererImpl {
         final String listName = "${propertyName}List"
 
         return { ->
-            table {
+            table(border:1, cellpadding:5, class:alibaba,){
                 thead {
                     tr {
                         properties.each {
